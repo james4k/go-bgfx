9c1350cc0b67e0dcb583784d80807957b0dc03ab
James Gray <james@james4k.com>
2014-05-29 21:41:17 -0500

    renamed src/ to shim/ for clarity

--- prepare.sh
+++ prepare.sh
@@ -16,12 +16,12 @@ cp -r lib/bx/include/* include/
 
 # concatenate all *.cpp files into one
 echo "// Generate file with prepare.sh" > bgfx.cpp
-for file in `ls lib/bgfx/src/*.cpp src/*.cpp`; do
+for file in `ls lib/bgfx/src/*.cpp shim/*.cpp`; do
 	cat $file >> bgfx.cpp
 done
 
 # concatenate all *.m (obj-c) files into one
 echo "// Generate file with prepare.sh" > bgfx.m
-for file in `ls src/*.m`; do
+for file in `ls shim/*.m`; do
 	cat $file >> bgfx.m
 done
